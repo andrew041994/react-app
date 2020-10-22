b8771325cdaa4dea2d9b32ee8c91cee23ab8bca0
andrew041994 <andrew041994@gmail.com>
2020-10-21 23:33:15 -0400

    manipulate state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
-import Person from './Person/Person'
+// import Person from './Person/Person'
 import './App.css';
 import UserOutput from './Person/User/UserOutput';
+import UserInput from './Person/User/UserInput';
 
 
 class App extends Component {
-state = {
-  users: ["andrew", "jake"]
- 
+constructor() {
+  super();
+  this.state = {
+    user1: "andrew",
+    user2: "jake"
+  }
 }
+
+
 
   render() {
     return (
       <div className="App">
      <h1>Hello World</h1>
-    <UserOutput userName={this.state.users[0]}/>
-    <UserOutput userName={this.state.users[1]}/>
-
+    <UserOutput userName={this.state.user1}/>
+    <UserInput change={(event)=> {this.setState({user1: event.target.value})}}/>
+    <UserOutput userName={this.state.user2}/>
+    <UserInput change={(event)=> {this.setState({user2: event.target.value})}}/>
       </div>
     );
   }
